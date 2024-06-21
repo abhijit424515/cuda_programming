77d8ecd226ed997373f5833e58e897816235e01e
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2024-06-21 09:56:11 +0530

    coalescing in matmul

---
+++ 2_matrix_mul/2_coalescing.cu
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+__global__ void matrixMul(const int *a, const int *b, int *c, int N) {
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+	c[row * N + col] = 0;
+	for (int k = 0; k < N; k++) {
+		c[row * N + col] += a[row * N + k] * b[col * N + k];
+	}
+}
+
+void transpose(vector<int> a, vector<int>& a_t, int N) {
+	for (int i=0; i<N; i++)
+		for (int j=0; j<N; j++)
+			a_t[j*N+i] = a[i*N+j];
+}
+
+void verify_result(vector<int> &a, vector<int> &b, vector<int> &c, int N) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int tmp = 0;
+			for (int k = 0; k < N; k++) {
+			tmp += a[i * N + k] * b[k * N + j];
+			}
+			assert(tmp == c[i * N + j]);
+		}
+	}
+}
+
+int main() {
+	constexpr int N = 1 << 10;
+	constexpr size_t bytes = N * N * sizeof(int);
+
+	// Host vectors
+	vector<int> h_a(N * N);
+	vector<int> h_b(N * N);
+	vector<int> h_c(N * N);
+
+	// Initialize matrices
+	std::generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+	std::generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
+
+	// Transpose B
+	vector<int> h_b_t(N * N);
+	transpose(h_b, h_b_t, N);
+
+	// Allocate device memory
+	int *d_a, *d_b, *d_c;
+	cudaMalloc(&d_a, bytes);
+	cudaMalloc(&d_b, bytes);
+	cudaMalloc(&d_c, bytes);
+
+	// Copy data to the device
+	cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, h_b_t.data(), bytes, cudaMemcpyHostToDevice);
+
+	int THREADS = 32;
+	int BLOCKS = N / THREADS;
+
+	// Use dim3 structs for block  and grid dimensions
+	dim3 threads(THREADS, THREADS);
+	dim3 blocks(BLOCKS, BLOCKS);
+
+	// Launch kernel
+	matrixMul<<<blocks, threads>>>(d_a, d_b, d_c, N);
+
+	cudaDeviceSynchronize();
+	std::cout << "GPU MATMUL FINISHED\n";
+
+	cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+	verify_result(h_a, h_b, h_c, N);
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+
+	std::cout << "COMPLETED SUCCESSFULLY\n";
+	return 0;
+}
